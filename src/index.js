5e5cf917416028b15264dfecac85f78f7e2a8942
iulia <iuliamarialungu@yahoo.com>
2019-10-31 11:38:34 +0100

    Mocked some predictions and a chosen place

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import './main.css';
 import { Elm } from './Main.elm';
 import * as serviceWorker from './serviceWorker';
 
+import mockedPredictions from './predictions.json'
+import mockedPlace from './place.json'
+
 const app = Elm.Main.init({
   node: document.getElementById('root')
 });
@@ -11,28 +14,39 @@ app.ports.logger.subscribe(message => {
 });
 
 app.ports.predictAddress.subscribe(function (text) {
-  if (!text) { return; }
-  var service = new google.maps.places.AutocompleteService();
-  var options = { input: text, types: ['address'] }
-  service.getPlacePredictions(options, function (predictions, status) {
-    console.log("Got these results", predictions, 'status', status);
-    // here predictions are sent back to Elm
-    if (status == 'OK') {
-      app.ports.addressPredictions.send(predictions);
-    } else app.ports.addressPredictions.send(status);
-  });
+  // if (!text) { return; }
+  // var service = new google.maps.places.AutocompleteService();
+  // var options = { input: text, types: ['address'] }
+  // service.getPlacePredictions(options, function (predictions, status) {
+  //   console.log("Got these results", predictions, 'status', status);
+
+  //   if (status == 'OK') {
+  //     app.ports.addressPredictions.send(predictions);
+  //   } else app.ports.addressPredictions.send(status);
+  // });
+
+
+  console.log("Got these predictions", mockedPredictions);
+  app.ports.addressPredictions.send(mockedPredictions);
+
+
 });
 
 
 app.ports.getPredictionDetails.subscribe(function (text) {
-  var request = { placeId: text };
-  var service = new google.maps.places.PlacesService(document.createElement('div'));
-  service.getDetails(request, function (place, status) {
-    console.log("Got this place", place, 'status', status);
-    if (status == 'OK') {
-      app.ports.addressPredictions.send(JSON.stringify(place));
-    } else app.ports.addressPredictions.send(status);
-  });
+  // var request = { placeId: text };
+  // var service = new google.maps.places.PlacesService(document.createElement('div'));
+  // service.getDetails(request, function (place, status) {
+  //   console.log("Got this place", place, 'status', status);
+  //   if (status == 'OK') {
+  //     app.ports.addressDetails.send(JSON.stringify(place));
+  //   } else app.ports.addressDetails.send(status);
+  // });
+
+
+  console.log("Got this place", mockedPlace);
+  app.ports.addressDetails.send(JSON.stringify(mockedPlace));
+
 });
 
 // If you want your app to work offline and load faster, you can change
